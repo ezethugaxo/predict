859448d766fd04f45ade28c786dc4a039fd819ee
Ezethu Gaxo <ezethugaxo@gmail.com>
2022-10-25 12:33:49 +0200

    first commit in deployed

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://2z8u84uk1a.execute-api.eu-west-1.amazonaws.com/default",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
