9aea277dcae559e92c99f1271d636a7e07066a2b
ezethugaxo <63429969+ezethugaxo@users.noreply.github.com>
2022-10-30 22:28:16 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://rohnjdw784.execute-api.eu-west-1.amazonaws.com/default/mylambdafunc",
+                url: "https://ewsx8pg1uh.execute-api.eu-west-1.amazonaws.com/default/mylambdafunc",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
